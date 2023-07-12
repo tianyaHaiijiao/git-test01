34ea491c9496148c5cd38b19c5e741d6de1fa6b6
tianyaHaiijiao <zl223043@163.com>
2023-07-12 09:55:12 +0800

    Initial commit

---
+++ src/com/lsj/test/HelloGitHub.java
@@ -0,0 +1,11 @@
+package com.lsj.test;
+
+/**
+ * @author 梁圣洁
+ * @version 1.0
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("hello,github!");
+    }
+}
